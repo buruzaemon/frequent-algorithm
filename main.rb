78c16b30bd482c3a5948b3d6d8362b67f5f016c3
buruzaemon <buruzaemon@gmail.com>
2015-03-14 00:19:03 +0900

    adding test skeleton, sample main.rb

---
+++ main.rb
@@ -0,0 +1,33 @@
+$: << 'lib'
+require 'frequent-algorithm'
+
+if __FILE__ == $0
+
+  # our basic parameters
+  N = 100  # size of main window
+  b =  20  # size of basic window
+  k =   3  # top-3 numerals of pi
+  
+  # prep our algorithm instance
+  alg = Frequent::Algorithm.new(N, b, k) 
+  
+  # read in the test data (pi to 1000 digits)
+  # data includes decimal, and some random whitespace
+  raw = File.read('test/frequent/test_data_pi').strip
+  raw.delete!('.').delete!(' ')
+  
+  # split the data into separate chars,
+  # and group into 1000/b arrays of b items each
+  # data is a Generator
+  data = raw.scan(/./).each_slice(b)
+  
+  # read in first basic window...
+  #alg.process(data.next)
+  
+  # check global statistics state
+  # puts alg.statistics
+  # check global delta value
+  # puts alg.delta
+  
+  puts "all done!"
+end
