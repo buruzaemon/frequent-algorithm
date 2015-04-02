b10a0ac70f7c35abf99fb12d547692691a1458e5
buruzaemon <buruzaemon@gmail.com>
2015-04-02 23:25:58 +0900

    Let us see if this dog will hunt.

--- demo/main.rb
+++ demo/main.rb
@@ -4,18 +4,24 @@ require 'frequent-algorithm'
 PUNCT = /(\.|\"|\'s|,|\?|!|;|:")/
 
 if __FILE__ == $0
+
+  alg = Frequent::Algorithm.new(500, 100, 5) 
+
   f = File.join(File.expand_path(File.dirname(__FILE__)),
                                  'corpora',
                                  'odyssey.mb.txt')
   enum = File.foreach(f)
   # the Odyssey begins at line 12 and spans 10412 lines
   12.times { enum.next }
-  10412.times do 
+  10412.times do |i|
     line = enum.next.strip.downcase
     words = line.gsub(PUNCT, '').split
     words.each do |w|
-      #puts w
       # at this point, feed in the word w into the process method
+      alg.process(w)
+      if i>1000 && i%1000==0 && rand
+        puts alg.report
+      end
     end
   end
 end
